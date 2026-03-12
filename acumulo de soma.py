4b620e7219e0b69dba3b9aef223296ea8943399f
Yuto <y.a.arakaki2008@gmail.com>
2026-03-12 19:22:48 -0300

    Add files via upload

---
+++ acumulo de soma.py
@@ -0,0 +1,20 @@
+soma = 0
+valor = input ("digite um numero: ")
+num = int(valor)
+soma = soma + num
+
+
+valor = input ("digite um numero: ")
+num = int(valor)
+soma = soma + num
+
+
+
+valor = input ("digite um numero: ")
+num = int (valor)
+soma = soma + num
+
+valor = input ("digite um numero: ")
+num = int (valor)
+soma = soma + num
+print(soma)
