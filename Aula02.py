7d917441ac760016a1a9c6dbdb5fc5d6ca00bacf
yutoinouee <y.a.arakaki2008@mail.com>
2026-03-12 19:14:35 -0300

    Aula python

---
+++ Aula02.py
@@ -0,0 +1,14 @@
+valor = input (" Digite um numero : ")
+numA = int ( valor )
+valor = input (" Digite um numero : ")
+numB = int ( valor )
+valor = input (" Digite um numero : ")
+numC = int ( valor )
+valor = input (" Digite um numero : ")
+numD = int ( valor )
+valor = input (" Digite um numero : ")
+numE = int ( valor )
+
+soma = numA + numB + numC + numD + numE
+
+print ( soma )
